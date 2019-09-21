912f56af22e8bf77366eadf0ad191241d2e703cd
cecelia23 <351613102@qq.com>
2019-09-21 14:38:34 +0800

    before wangyi2

--- scripts/algorithm/quickSort.js
+++ scripts/algorithm/quickSort.js
@@ -3,17 +3,17 @@ function quickSort (start, end) {
     return;
   }
   let temp=arr[start],t,i=start,j=end;
-  while (i!=j){
-    while(arr[j]>=temp&&i<j){
+  while (i != j){
+    while(arr[j] >= temp && i < j) {
       j--;   // 从右边扫描，找到比temp小的
     }
-    while(arr[i]<=temp&&i<j){
+    while(arr[i] <= temp && i < j) {
       i++;   // 从左边扫描，找到比temp大的
     }
-    if (i!=j){
-      t=arr[i];
-      arr[i]=arr[j];
-      arr[j]=t;
+    if (i != j) {
+      t = arr[i];
+      arr[i] = arr[j];
+      arr[j] = t;
     }
     // 交换i,j位置处的值后，继续扫描
   }
@@ -25,5 +25,5 @@ function quickSort (start, end) {
 
 var arr = [2,5,9,0,4,8,7,1,3,6];
 // console.log(arr[0],arr[arr.length-1])
-quickSort(0,arr.length-1);
+quickSort(0, arr.length-1);
 console.log(arr)
